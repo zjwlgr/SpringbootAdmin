b520b06e818071bc799eff22da02482bbb6d25af
zjwlgr <zhangjian@kuaxue.com>
2017-08-02 15:15:20 +0800

    up

---
+++ src/main/java/com/brander/BranderApplication.java
@@ -0,0 +1,12 @@
+package com.brander;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BranderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BranderApplication.class, args);
+	}
+}
