f28696ca97c2409b9d51f6096e7e434b80658450
zjwlgr <zjwlgr@yeah.net>
2017-08-03 23:27:00 +0800

    layou之前提交

--- src/main/java/com/brander/admin/controller/LoginController.java
+++ src/main/java/com/brander/admin/controller/LoginController.java
@@ -1,8 +1,12 @@
 package com.brander.admin.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * 管理员登录
@@ -12,7 +16,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class LoginController {
 
     @GetMapping(value = "/login")
-    public String login(){
+    public String login(ModelMap map){
+        SimpleDateFormat df = new SimpleDateFormat("yyyy");
+        String year = df.format(new Date());
+        map.addAttribute("year",year);
         return "admin/login/index";
     }
 
