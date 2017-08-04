98f0693c5f9ffb09949968c7cf0840b310c8ea0e
zjwlgr <zhangjian@kuaxue.com>
2017-08-04 17:54:20 +0800

    up

--- src/main/java/com/brander/admin/controller/LoginController.java
+++ src/main/java/com/brander/admin/controller/LoginController.java
@@ -7,9 +7,6 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
 /**
  * 管理员登录
  */
@@ -17,15 +14,10 @@ import java.util.Date;
 @RequestMapping(value = "/admin")
 public class LoginController {
 
-    @Autowired
-    MyConfigProperties myConfigProperties;
 
     @GetMapping(value = "/login")
-    public String login(ModelMap map){
+    public String login(){
 
-
-
-        map.addAttribute("myconfig",myConfigProperties);
         return "admin/login/index";
     }
 
