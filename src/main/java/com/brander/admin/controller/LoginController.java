4c036bbe90ec02d8999a1e840ca83b631e0adae9
zjwlgr@yeah.net <zjwlgr47782906>
2017-08-05 08:57:29 +0800

    up

--- src/main/java/com/brander/admin/controller/LoginController.java
+++ src/main/java/com/brander/admin/controller/LoginController.java
@@ -3,7 +3,6 @@ package com.brander.admin.controller;
 import com.brander.common.properties.MyConfigProperties;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
