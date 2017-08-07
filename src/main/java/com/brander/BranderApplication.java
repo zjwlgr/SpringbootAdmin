00ee7e7be6551181d3e978c1e4f6381d540bc7df
zjwlgr <zjwlgr@yeah.net>
2017-08-07 21:05:48 +0800

    自动生成实体类

--- src/main/java/com/brander/BranderApplication.java
+++ src/main/java/com/brander/BranderApplication.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
 @PropertySource("classpath:jdbc.properties") //可加载多个配置文件
-@MapperScan("cn.form1.mapper") //指定mybatis的Mapper的包路径
+@MapperScan("com.brander.common.mapper") //指定mybatis的Mapper的包路径
 public class BranderApplication {
 
 	public static void main(String[] args) {
