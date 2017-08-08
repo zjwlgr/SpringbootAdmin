c6c26a37383b0464d739f8f4b22c4359d398ccd8
zjwlgr <zhangjian@kuaxue.com>
2017-08-08 18:00:24 +0800

    up

---
+++ src/main/java/com/brander/Test.java
@@ -0,0 +1,19 @@
+package com.brander;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Date;
+
+/**
+ * 各种测试
+ */
+public class Test {
+
+    public static void main(String args[]) {
+
+        System.out.println(new Date().getTime());
+
+
+        System.out.println(System.currentTimeMillis()/1000);
+    }
+
+}
